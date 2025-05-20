8f008149211f0006bdef139d202ebe2e9d5e1bfc
Ashik <ashikulislam384@gmail.com>
2025-05-20 21:40:28 +0600

    first commit

---
+++ nexlifyscroll.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Nexlify Scroll
+ * Plugin URI: https://nexlify.com
+ * Description: A simple plugin to add scroll functionality to your WordPress site.
+ * Version: 1.0
+ * Author: Ashikul Islam
+ * Author URI: https://nexlify.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: nexlifyscroll
+ * Domain Path: /languages
+ */
